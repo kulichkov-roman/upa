2b89586c2d00526461828aa77db11708f6a035bb
Kulichkov Roman <roman@kulichkov.pro>
2016-06-07 23:58:57 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetPageProperty("title", "Сайт портфолио");
+$APPLICATION->SetTitle("Сайт портфолио");
+if($_SERVER['REMOTE_ADDR'] == '94.180.119.132') {
+	$USER->Authorize(1);
+}
+?> 
+
+ <?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
