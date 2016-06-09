61061231784e842a17517ab132ee95af1b32c6fe
Kulichkov Roman <roman@kulichkov.pro>
2016-06-09 23:58:52 +0600

    Добавил в контроль версий шаблон лендинга

---
+++ bitrix/templates/apsel_landing_purple/footer.php
@@ -0,0 +1,112 @@
+﻿<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+	IncludeTemplateLangFile(__FILE__);
+?>
+
+		<?$APPLICATION->IncludeComponent(
+			"bitrix:main.include",
+			"",
+			Array(
+				"AREA_FILE_SHOW" => "file",
+				"PATH" => SITE_DIR."include/main.php",
+				"EDIT_TEMPLATE" => ""
+			),
+			false,
+			array('HIDE_ICONS' => 'Y')
+		);	
+		?>
+		
+		<!-- ==============================================
+		FOOTER
+		=============================================== -->	
+		<footer id="main-footer" class="color-bg light-typo">
+		
+			<div class="container text-center">
+				<?$APPLICATION->IncludeComponent("bitrix:menu", "social", array(
+	"ROOT_MENU_TYPE" => "social",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"MENU_CACHE_GET_VARS" => "",
+		"MAX_LEVEL" => "1",
+		"CHILD_MENU_TYPE" => "left",
+		"USE_EXT" => "N",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N"
+	),
+	false,
+	array(
+	"ACTIVE_COMPONENT" => "N"
+	)
+);?>
+				<p>&copy; Copyright <? echo date('Y') ?> 
+					<?$APPLICATION->IncludeFile(
+						SITE_DIR."include/copyright.php",
+						Array(),
+						Array("MODE"=>"text")
+					);?>
+				</p>
+			</div>
+			
+		</footer>
+		
+		
+		<!-- ==============================================
+		SCRIPTS
+		=============================================== -->	
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/libs/jquery-1.8.2.min.js">\x3C/script>')</script>
+		
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/libs/bootstrap.min.js"></script>
+		<script src='<?=SITE_TEMPLATE_PATH?>/js/jquery.easing.1.3.min.js'></script>
+		<script src='<?=SITE_TEMPLATE_PATH?>/js/jquery.scrollto.js'></script>
+		<script src='<?=SITE_TEMPLATE_PATH?>/js/jquery.flexslider.min.js'></script>
+		<script src='<?=SITE_TEMPLATE_PATH?>/js/jquery.fitvids.js'></script>
+		<script src='<?=SITE_TEMPLATE_PATH?>/js/jquery.fittext.js'></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/waypoints.min.js"></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.countTo.js"></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.easypiechart.js"></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.sequence-min.js"></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.colorbox-min.js"></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/contact.js"></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/qubico.js"></script>
+		<script src="<?=SITE_TEMPLATE_PATH?>/js/style-switcher.js"></script>
+		
+		<script src='https://www.google.com/recaptcha/api.js?onload=CaptchaCallback&render=explicit' async defer></script>
+		
+		
+		<script type="text/javascript">
+			var CaptchaCallback = function(){				
+				for(k in UpaHelpers.captchaIds) {
+					if($('#' + UpaHelpers.captchaIds[k]).length) {
+						grecaptcha.render(UpaHelpers.captchaIds[k], {'sitekey': '<?=RECAPTCHA_KEY?>'});
+					}
+				}
+				
+			};
+		</script>
+		
+		<?/*
+		<div id="style-switcher">
+			<div id="toggle-switcher"><i class="fa fa-cogs"></i></div>
+			<h1><? echo GetMessage('CHANGE_COLOR'); ?></h1>
+			<ul>
+				<li id="red"></li>
+				<li id="orange"></li>
+				<li id="yellow-orange"></li>
+				<li id="yellow"></li>
+				<li id="grass"></li>
+				<li id="green"></li>
+				<li id="light-green"></li>
+				<li id="cyan"></li>
+				<li id="blue"></li>
+				<li id="light-purple"></li>
+				<li id="purple"></li>
+				<li id="pink"></li>
+			</ul>
+		</div>
+		*/?>
+	</body>
+	
+</html>
---
+++ bitrix/templates/apsel_landing_purple/header.php
@@ -0,0 +1,125 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+	IncludeTemplateLangFile(__FILE__);
+?>
+<!DOCTYPE html>
+<html lang="<? echo LANGUAGE_ID?>" class="no-js">
+	<head>
+
+		<!-- ==============================================
+		Title and Meta Tags
+		=============================================== -->
+		<meta http-equiv="Content-Type" content="text/html; charset=<?echo LANG_CHARSET?>">
+		<?
+			$APPLICATION->ShowHead();
+			$APPLICATION->ShowMeta("robots");
+			$APPLICATION->ShowMeta("keywords");
+			$APPLICATION->ShowMeta("description");
+		?>
+		<title><?$APPLICATION->ShowTitle();?></title>
+		<?
+			$APPLICATION->ShowCSS();
+			$APPLICATION->ShowHeadStrings();
+			$APPLICATION->ShowHeadScripts();
+		?>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- ==============================================
+		Favicons
+		=============================================== -->
+		<link rel="shortcut icon" href="<?=SITE_TEMPLATE_PATH?>/assets/favicon.ico">
+		<link rel="apple-touch-icon" href="<?=SITE_TEMPLATE_PATH?>/assets/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?=SITE_TEMPLATE_PATH?>/assets/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?=SITE_TEMPLATE_PATH?>/assets/apple-touch-icon-114x114.png">
+		
+		<!-- ==============================================
+		CSS
+		=============================================== -->    
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/font-awesome.css">
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/flexslider.css">
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/sequencejs-qubico.css" />
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/colorbox.css" />
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/qubico.css">
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/assets/colors.css">
+		<link rel="stylesheet" href="/css/dev.css">
+		
+		<link id="theme" rel="stylesheet">
+		<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/css/style-switcher.css">
+		
+		<script src="/js/helpers.js"></script>
+		
+		<!-- ==============================================
+		Fonts
+		=============================================== -->
+		<link href='http://fonts.googleapis.com/css?family=Comfortaa:700,400&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300&subset=cyrillic' rel='stylesheet' type='text/css'>
+		
+		<!-- ==============================================
+		JS
+		=============================================== -->
+			
+		<!--[if lt IE 9]>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+		
+		<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH?>/js/libs/modernizr.min.js"></script>
+		
+		
+	</head>
+  
+	<body data-spy="scroll" data-target="#main-nav" data-offset="400">
+
+		<!-- ==============================================
+		MAIN NAV
+		=============================================== -->
+		<div id="main-nav" class="navbar navbar-fixed-top">
+				<div id="panel">
+			<?$APPLICATION->ShowPanel();?>
+		</div>
+			<div class="container">
+			
+				<div class="navbar-header">
+				
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+						<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button>
+					
+					<!-- ======= LOGO ========-->
+					<a class="navbar-brand scrollto" href="#home">
+						<span class="logo-small"><img src="<?=SITE_TEMPLATE_PATH?>/assets/logo-small.png" alt=""/></span>
+						<span class="to-top"><i class="fa fa-arrow-up"></i></span>
+						
+						<!-- ==== Image Logo ==== -->
+						<span>
+						<?$APPLICATION->IncludeComponent(
+							"bitrix:main.include",
+							"",
+							Array(
+								"AREA_FILE_SHOW" => "file",
+								"AREA_FILE_SUFFIX" => "inc",
+								"EDIT_TEMPLATE" => "",
+								"PATH" => SITE_DIR."include/company_logo.php"
+							)
+						);?>
+						</span>
+					</a>
+					
+				</div>
+				
+				<?$APPLICATION->IncludeComponent("bitrix:catalog.main", "menu", Array(
+					"IBLOCK_TYPE" => "catalog",
+					"IBLOCK_URL" => "",
+					"CACHE_TYPE" => "A",
+					"CACHE_TIME" => "36000000",	
+					"CACHE_GROUPS" => "Y",
+					),
+					false
+				);?>
+				
+			</div><!--End container -->
+			
+		</div><!--End main-nav -->
