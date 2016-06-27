df41a1e96aae7db2cce8f5eb4f11c8820ce65c2a
Kulichkov Roman <roman@kulichkov.pro>
2016-06-28 02:15:29 +0600

    Правки контактов

--- include/contacts.php
+++ include/contacts.php
@@ -8,11 +8,11 @@
         <h2>Новосибирск</h2>
         <p>ул. Крылова, д. 36, оф. 521б</p>
         <a href="mailto:sale@agency-up.ru">sale@agency-up.ru</a>
-        <p>+7 (383) 252-44-44</p>
-    </div><?/*?><div class="address">
+        <p>+7 (383) 312-02-96</p>
+    </div><div class="address">
         <h2>Барнаул</h2>
-        <p>ул. Социалистический проспект, д. 105</p>
+        <p>пр. Красноармейский, д. 106</p>
         <a href="mailto:sale@agency-up.ru">sale@agency-up.ru</a>
-        <p>+7 (905) 939-66-33</p>
-    </div><?*/?>
+        <p>+7 (383) 312-02-96</p>
+    </div>
 </div>
