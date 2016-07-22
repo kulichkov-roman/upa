3a1641bafc8a481b58dfffa04701dc115c942a95
Roman Kulichkov <roman@kulichkov.pro>
2016-07-22 14:28:51 +0600

    Замена адреса, убрал Барнаул

--- include/contacts.php
+++ include/contacts.php
@@ -4,14 +4,10 @@
         <p>ул. Сущевский Вал, д. 43</p>
         <a href="mailto:sale@agency-up.ru">sale@agency-up.ru</a>
         <p>+7 (495) 369-17-42</p>
-    </div><div class="address">
+    </div>
+    <div class="address">
         <h2>Новосибирск</h2>
         <p>ул. Крылова, д. 36, оф. 521б</p>
-        <a href="mailto:sale@agency-up.ru">sale@agency-up.ru</a>
-        <p>+7 (383) 312-02-96</p>
-    </div><div class="address">
-        <h2>Барнаул</h2>
-        <p>пр. Красноармейский, д. 106</p>
         <a href="mailto:sale@agency-up.ru">sale@agency-up.ru</a>
         <p>+7 (383) 312-02-96</p>
     </div>
